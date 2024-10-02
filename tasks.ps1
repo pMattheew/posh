819c0eaa10ad1dfb3e0a707c6964c1ab56bef50c
Patrick M <patrickmatias.silva@hotmail.com>
2024-10-02 17:16:27 -0300

    refactor: remove e2e tests from ci pipeline

--- tasks.ps1
+++ tasks.ps1
@@ -9,9 +9,11 @@ if (-not (Get-Alias tsk -ErrorAction SilentlyContinue)) {
 
 if (-not (Test-Path ".env")) { cp .env.example .env; Write-Host "Copied .env.example to .env" }
 
-function Test { Invoke-Pester -Path .\tests -Output Detailed }
+function Test { Invoke-Pester -Path .\tests -Output Detailed -ExcludeTag "e2e" }
+function TestE2E { Invoke-Pester -Path .\tests -Output Detailed }
 
 switch ($task.ToUpper()) {
     { $_ -in 'T', 'TEST' } { Test; break }
+    'TEST:E2E' { TestE2E }
     Default { echo "`nUsage: tsk task`n" }
 }
--- tests/app-e2e.tests.ps1
+++ tests/app-e2e.tests.ps1
@@ -2,7 +2,7 @@ BeforeAll {
     "$PSScriptRoot\..\tasks.ps1" | Invoke-Expression
 }
 
-Describe "App" {
+Describe "App" -Tag "e2e" {
     It "should initialize" {
         "$PSScriptRoot\..\bootstrap.ps1" | Invoke-Expression 
         $app | Should -Not -BeNullOrEmpty
