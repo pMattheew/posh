d3a6ceedd4e531e6fbdc831d90b5f0c4b5e56c61
Patrick M <patrickmatias.silva@hotmail.com>
2024-10-04 14:47:21 -0300

    feat: start app with copy of `.env.example` if `.env` isn't present

--- .env.ps1
+++ .env.ps1
@@ -9,17 +9,18 @@ if (-not (Test-Path $env:ROOT)) {
     New-Item -Path $env:ROOT -ItemType Directory > $null
 }
 
-if (Test-Path $path) {
-    Get-Content $path | ForEach-Object {
+if (-not (Test-Path $path)) {
+    Write-Warning "The .env file does not exist."
+    Write-Warning "Proceeding with a copy of .env.example. in $PSScriptRoot"
+    Copy-Item "$PSScriptRoot\.env.example" ".env"
+}
 
-        $parts = $_ -split '='
-        if ($parts.Length -eq 2) {
-            $key = $parts[0].Trim()
-            $value = $parts[1].Trim()
-            Set-Item -Path "env:$key" -Value $value
-        }
+Get-Content $path | ForEach-Object ({
+    $parts = $_ -split '='
+    if ($parts.Length -eq 2) {
+        $key = $parts[0].Trim()
+        $value = $parts[1].Trim()
+        Set-Item -Path "env:$key" -Value $value
     }
-}
-else {
-    Write-Error "The .env file does not exist."
-}
+})
+
