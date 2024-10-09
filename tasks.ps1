bde97dfa33b0f7aad3d4af9f2d06007ad6fc8655
Patrick M <patrickmatias.silva@hotmail.com>
2024-10-09 15:45:13 -0300

    fix(tasks): use `$PSScriptRoot` in all paths

--- tasks.ps1
+++ tasks.ps1
@@ -3,14 +3,14 @@ param(
 )
 
 if (-not (Get-Alias tsk -ErrorAction SilentlyContinue)) {
-    Set-Alias -Name tsk -Value "$PSScriptRoot/tasks.ps1" -Scope Global
+    Set-Alias -Name tsk -Value "$PSScriptRoot\tasks.ps1" -Scope Global
     Write-Host "`nNow you can use `"tsk`" to run tasks."
 }
 
 if (-not (Test-Path ".env")) { cp .env.example .env; Write-Host "Copied .env.example to .env" }
 
-function Test { Invoke-Pester -Path .\tests -Output Detailed -ExcludeTag "e2e" }
-function TestE2E { Invoke-Pester -Path .\tests -Output Detailed }
+function Test { Invoke-Pester -Path "$PSScriptRoot\tests" -Output Detailed -ExcludeTag "e2e" }
+function TestE2E { Invoke-Pester -Path "$PSScriptRoot\tests" -Output Detailed }
 function LocalTest { "$PSScriptRoot\app-example.ps1" | Invoke-Expression }
 
 switch ($task.ToUpper()) {
